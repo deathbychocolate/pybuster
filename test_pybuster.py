053d2fe31bdc7126189614dab59bada621752cef
Damianos Vonapartis <damianosvon@gmail.com>
2023-01-21 22:34:02 +0100

    Add pytest marks for tests
    Add 2 more tests for index_file

--- test_pybuster.py
+++ test_pybuster.py
@@ -1,11 +1,12 @@
 """File containing tests
 """
 
+import pytest
 import pybuster as pb
 import helper
-import pytest
 
 
+@pytest.mark.count_lines
 def test_count_lines_should_count_0_lines_in_file():
     error_message = """
     Number of lines do not equal 0.
@@ -13,6 +14,7 @@ def test_count_lines_should_count_0_lines_in_file():
     assert pb.count_lines(helper.FILEPATH_TEST_FILES_0_LINE_FILE) == 0, error_message
 
 
+@pytest.mark.count_lines
 def test_count_lines_should_count_1_lines_in_file():
     error_message = """
     Number of lines do not equal 1.
@@ -20,6 +22,7 @@ def test_count_lines_should_count_1_lines_in_file():
     assert pb.count_lines(helper.FILEPATH_TEST_FILES_1_LINE_FILE) == 1, error_message
 
 
+@pytest.mark.count_lines
 def test_count_lines_should_count_10_lines_in_file():
     error_message = """
     Number of lines do not equal 10.
@@ -27,8 +30,30 @@ def test_count_lines_should_count_10_lines_in_file():
     assert pb.count_lines(helper.FILEPATH_TEST_FILES_10_LINE_FILE) == 10, error_message
 
 
+@pytest.mark.count_lines
 def test_count_lines_should_count_100_lines_in_file():
     error_message = """
     Number of lines do not equal 100.
     """
     assert pb.count_lines(helper.FILEPATH_TEST_FILES_100_LINE_FILE) == 100, error_message
+
+
+@pytest.mark.index_file
+def test_should_return_an_empty_dictionary_with_no_keys_when_passing_an_empty_file():
+    error_message = """
+    Keys were detected in a dictionary about an empty file.
+    """
+    assert len(pb.index_file(helper.FILEPATH_TEST_FILES_0_LINE_FILE).keys()) == 0, error_message
+
+
+@pytest.mark.index_file
+def test_should_return_an_dictionary_with_no_values_when_passing_an_empty_file():
+    error_message = """
+    Values were detected in a dictionary about an empty file.
+    """
+    assert len(pb.index_file(helper.FILEPATH_TEST_FILES_0_LINE_FILE).values()) == 0, error_message
+
+
+# @pytest.mark.index_file
+# @pytest.mark.index_file
+# @pytest.mark.index_file
