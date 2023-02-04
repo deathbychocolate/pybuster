b05ef5617caf3f821dfba57083add0a52715a9cb
Damianos Vonapartis <damianosvon@gmail.com>
2023-02-04 18:53:14 +0100

    Remove useless tests
    Test that raises value error when file is empty

--- test_pybuster.py
+++ test_pybuster.py
@@ -39,19 +39,9 @@ def test_count_lines_should_count_100_lines_in_file():
 
 
 @pytest.mark.index_file
-def test_should_return_an_empty_dictionary_with_no_keys_when_passing_an_empty_file():
-    error_message = """
-    Keys were detected in a dictionary about an empty file.
-    """
-    assert len(pb.index_file(helper.FILEPATH_TEST_FILES_0_LINE_FILE).keys()) == 0, error_message
-
-
-@pytest.mark.index_file
-def test_should_return_an_dictionary_with_no_values_when_passing_an_empty_file():
-    error_message = """
-    Values were detected in a dictionary about an empty file.
-    """
-    assert len(pb.index_file(helper.FILEPATH_TEST_FILES_0_LINE_FILE).values()) == 0, error_message
+def test_should_raise_a_value_error_when_the_filepath_is_an_empty_file():
+    with pytest.raises(ValueError):
+        pb.index_file(helper.FILEPATH_TEST_FILES_0_LINE_FILE).values()
 
 
 # @pytest.mark.index_file
