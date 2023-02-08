11a5985e29e8ee721fa2896991b2f550792836b4
Damianos Vonapartis <damianosvon@gmail.com>
2023-02-08 21:59:24 +0100

    Format python files using black formmater

--- pybuster.py
+++ pybuster.py
@@ -69,7 +69,8 @@ def assign_indexes(filepath: str, thread_count: int) -> list:
     file_thread_indexes = []
     count = 1  # start at 1 so that we treat the last thread differently
     lines_per_thread, lines_per_thread_last = assign_per_thread_work(
-        line_count, thread_count)
+        line_count, thread_count
+    )
 
     logging.info("assigning indexes to each thread...")
     while count < thread_count:
@@ -80,9 +81,7 @@ def assign_indexes(filepath: str, thread_count: int) -> list:
         start_index = end_index
         count = count + 1
 
-    logging.info(
-        "assigning our calculated average to the last thread + remainder"
-    )
+    logging.info("assigning our calculated average to the last thread + remainder")
     start_index = end_index + 1
     end_index = end_index + lines_per_thread_last
     file_thread_indexes.append((start_index, end_index))
@@ -95,12 +94,12 @@ def http_get(http_get_parameters):
     Perform simple GET request using requests module.
     """
     indexed_wordlist, wordlist_indexes, target_webpage = http_get_parameters
-    target_webpage = bytes(target_webpage, 'utf8')
+    target_webpage = bytes(target_webpage, "utf8")
     start_index, end_index = wordlist_indexes
 
     while start_index <= end_index:
         target_directory = indexed_wordlist[start_index]
-        target_full = b''.join([target_webpage, target_directory])
+        target_full = b"".join([target_webpage, target_directory])
         response = requests.get(target_full, timeout=10, allow_redirects=False)
         if response.status_code in STATUS_CODES_POSITIVE:
             print(f"GET {response.status_code} {target_full.decode()}")
@@ -157,7 +156,7 @@ def handle_user_input() -> argparse.Namespace:
         logging.critical("make sure to include the protocol (http[s]://)")
         sys.exit(1)
     if not arguments.url.endswith(URL_FORMAT_BACKSLASH):
-        arguments.url = ''.join([arguments.url, URL_FORMAT_BACKSLASH])
+        arguments.url = "".join([arguments.url, URL_FORMAT_BACKSLASH])
 
     # set default values
     if arguments.threads is None:
@@ -175,11 +174,7 @@ def run(args: argparse.Namespace) -> None:
     file_thread_indexes = assign_indexes(args.wordlist, args.threads)
     with ThreadPoolExecutor(args.threads) as executor:
         for i in range(args.threads):
-            http_get_parameters = (
-                wordlist_indexed,
-                file_thread_indexes[i],
-                args.url
-            )
+            http_get_parameters = (wordlist_indexed, file_thread_indexes[i], args.url)
             executor.submit(http_get, http_get_parameters)
 
 
@@ -191,5 +186,5 @@ def main() -> None:
     run(args)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
